18a1e6cd209c9d4c9a58c1a26f5eca95d2299114
ganziyan <337094393@qq.com>
2018-04-03 12:27:02 +0800

    运单导入

--- bos_management/bos_management_domain/src/main/java/com/itheima/bos/domain/take_delivery/WayBill.java
+++ bos_management/bos_management_domain/src/main/java/com/itheima/bos/domain/take_delivery/WayBill.java
@@ -20,7 +20,7 @@ public class WayBill {
     @Id
     @GeneratedValue
     @Column(name = "C_ID")
-    private Long id;
+    private Long id;      //1 id
     @Column(name = "C_WAY_BILL_NUM", unique = true)
     private String wayBillNum; // 运单编号
     @OneToOne
@@ -42,7 +42,7 @@ public class WayBill {
     @Column(name = "C_REC_NAME")
     private String recName;// 收件人姓名
     @Column(name = "C_REC_MOBILE")
-    private String recMobile;// 收件人电话
+    private String recMobile;//10 收件人电话
     @Column(name = "C_REC_COMPANY")
     private String recCompany;// 收件人公司
     @OneToOne
@@ -54,7 +54,7 @@ public class WayBill {
     @Column(name = "C_SEND_PRO_NUM")
     private String sendProNum; // 快递产品类型编号：速运当日、速运次日、速运隔日
     @Column(name = "C_GOODS_TYPE")
-    private String goodsType;// 托寄物类型：文件、衣服 、食品、电子商品
+    private String goodsType;//15 托寄物类型：文件、衣服 、食品、电子商品
     @Column(name = "C_PAY_TYPE_NUM")
     private String payTypeNum;// 支付类型编号：寄付日结、寄付月结、到付
     @Column(name = "C_WEIGHT")
@@ -65,7 +65,7 @@ public class WayBill {
     private Integer num; // 原件数
 
     @Column(name = "C_ARRIVE_CITY")
-    private String arriveCity; // 到达地
+    private String arriveCity; //20 到达地
 
     @Column(name = "C_FEEITEMNUM")
     private Integer feeitemnum; // 实际件数
@@ -77,7 +77,7 @@ public class WayBill {
     private String floadreqr; // 配载要求 (比如录入1=无，2=禁航，4=禁航空铁路)
 
     @Column(name = "C_WAY_BILL_TYPE")
-    private String wayBillType; // 运单类型（类型包括：正常单据、异单、调单）
+    private String wayBillType; //25  运单类型（类型包括：正常单据、异单、调单）
     /*
      * 运单状态： 1 待发货、 2 派送中、3 已签收、4 异常
      */
@@ -88,7 +88,7 @@ public class WayBill {
      * 1、新增修改单据状态为“否” 2、作废时需将状态置为“是” 3、取消作废时需要将状态置为“否”
      */
     @Column(name = "C_DELTAG")
-    private String delTag; // 作废标志
+    private String delTag; //  作废标志
 
     public String getArriveCity() {
         return arriveCity;
--- bos_management/bos_management_service/src/main/java/com/itheima/bos/service/take_delivery/WayBillService.java
+++ bos_management/bos_management_service/src/main/java/com/itheima/bos/service/take_delivery/WayBillService.java
@@ -1,5 +1,10 @@
 package com.itheima.bos.service.take_delivery;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.itheima.bos.domain.take_delivery.WayBill;
 
 /**  
@@ -10,6 +15,10 @@ import com.itheima.bos.domain.take_delivery.WayBill;
 public interface WayBillService {
 
     void save(WayBill model);
+
+	void save(List<WayBill> list);
+
+	Page<WayBill> findAll(Pageable pageable);
 
 }
   
